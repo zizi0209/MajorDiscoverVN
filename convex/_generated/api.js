e2dbea89ae2d93362d3585ecead3aa2c74d63a9d
Zyth <112655107+zizi0209@users.noreply.github.com>
2026-05-05 18:17:01 +0700

    chore: generate Convex API and schema definition files

--- convex/_generated/api.js
+++ convex/_generated/api.js
@@ -1,8 +1,23 @@
+/* eslint-disable */
 /**
  * Generated `api` utility.
- * Run `npx convex dev` to regenerate this file.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
  */
-import { anyApi } from "convex/server";
 
+import { anyApi, componentsGeneric } from "convex/server";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export const api = anyApi;
 export const internal = anyApi;
+export const components = componentsGeneric();
